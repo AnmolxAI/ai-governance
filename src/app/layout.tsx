6f49b8139aff77aa0190e0edcf64f7e372835c76
Anmol Kumar <41753338+AnmolxAI@users.noreply.github.com>
2025-11-05 22:41:22 -0500

    Update layout.tsx
    added aigt-logo.svg favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Global AI Governance Timeline',
   description: 'A filterable timeline of global AI regulations and policies.',
+  icons: {
+    icon: '/public/aigt-logo.svg', 
+  },
 };
 
 export default function RootLayout({
